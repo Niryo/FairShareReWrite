51fe004577cd0f47fea3127efb905df46293241b
Nir Yosef <niryo@wix.com>
2017-09-22 16:07:52 +0300

    first commit

---
+++ app/src/main/java/fair/share/fairshare/MainActivity.java
@@ -0,0 +1,13 @@
+package fair.share.fairshare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
